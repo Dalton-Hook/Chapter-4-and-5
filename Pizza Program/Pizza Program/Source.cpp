9025ef91c963ffd9d93c0b54e73810ce68008b72
Dalton Hook <35705638+Dalton-Hook@users.noreply.github.com>
2018-04-12 12:30:45 -0500

    update

---
+++ Pizza Program/Pizza Program/Source.cpp
@@ -0,0 +1,44 @@
+//Pizza Program
+//Author: Dalton Hook
+//Date: 4/12/2018
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	double total;
+	int userchoice;//Choice of pizza
+	double pizzaSmall = 7.99, pizzaMeduim = 8.99, pizzaLarge = 10.99;//Prices
+	int pizzaSize; //The size of the pizza
+	std::string pizzaString = NULL;
+
+	std::cout << " Welcome to Luigi's Pizza Joint!" << std::endl;
+	
+	std::cout << "What size of pizza do you want?" << std::endl;
+	std::cin >> pizzaSize;
+
+	
+	if (pizzaSize == 1)
+	{
+		std::cout << "you chose a pizza price is" << pizzaSmall << "what toppings?" << std::endl;
+		if (userchoice == 1)
+		{
+			//peperoni
+		}
+		else if (userchoice == 2)
+		{
+			// Bacon
+		}
+		else if (userchoice == 3)
+		{
+			//cheese
+		}
+		
+	}
+
+	std::cout << "your total a" << pizzaString << "is" << total;
+
+	system("pause");
+	return 0;
+}
